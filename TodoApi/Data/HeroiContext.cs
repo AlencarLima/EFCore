e72ffe172b8ca51eaa76ef6cd0bb59a0594b0862
Carlos Alencar Lima <carlos.alencar.99@gmail.com>
2022-02-19 23:12:08 -0300

    Add project files.

---
+++ TodoApi/Data/HeroiContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApi.Models;
+
+namespace TodoApi.Models
+{
+    public class HeroiContext : DbContext
+    {
+        public DbSet<Heroi> Herois { get; set; }
+        public DbSet<Batalha> Batalhas { get; set; }
+        public DbSet<Arma> Armas { get; set; }
+        public DbSet<HeroiBatalha> HeroisBatalhas { get; set; }
+        public DbSet<Identidadesecreta> IdentidadesSecretas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Password=sa1234;Persist Security Info=True;User ID=sa;Initial Catalog=HeroApp;Data Source=LAPTOP-6MPOSG31");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HeroiBatalha>(entity =>
+            {
+                entity.HasKey(e => new { e.BatalhaId, e.HeroiId });
+            });
+        }
+    }
+
+}
---
+++ TodoApi/Models/HeroiBatalha.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApi.Models;
+
+namespace TodoApi.Models
+{
+    public class HeroiBatalha
+    {
+        public int HeroiId { get; set; }
+        public Heroi Heroi { get; set; }
+        public int BatalhaId { get; set; }
+        public Batalha Batalha { get; set; }
+    }
+}
